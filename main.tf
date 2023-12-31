fd20a2dd95de6f3bca0bc7d5c02a3b31af078b21
VIVESH <35293085+574n13y@users.noreply.github.com>
2023-12-31 20:15:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "ec2" {
+resource "aws_instance" "my_ec2" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   key_name               = var.key_name
