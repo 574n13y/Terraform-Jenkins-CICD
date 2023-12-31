f7a0b4456b6f32cb694b8ce4c538557fbff076d9
VIVESH <35293085+574n13y@users.noreply.github.com>
2023-12-31 18:43:15 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,10 @@
 terraform {
   backend "s3" {
     bucket         = "ss-cicd-bucket"
-    key            = "my-terraform-environment/main"
+    key            = "my-terraform-environment"
     region         = "ap-northeast-1"
     dynamodb_table = "ss-dynamo-db-table"
+    assume_role = {
+      role_arn = "arn:aws:iam::PRODUCTION-ACCOUNT-ID:role/Terraform"
   }
 }
